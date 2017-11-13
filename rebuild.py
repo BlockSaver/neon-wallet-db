f57924c12452238b176f23c4949a987d5f3b1c0b
morrigan <belma.dzi@gmail.com>
2017-11-13 10:46:26 +0100

    Change blockheight

--- rebuild.py
+++ rebuild.py
@@ -3,7 +3,7 @@ from rq import Queue
 from api import redis_db as conn
 from api.blockchain import storeLatestBlockInDB, getBlockCount, blockchain_db, storeBlockInDB, checkSeeds, get_highest_node
 
-blockheight = 1181
+blockheight = 20044
 
 for i in range(0,blockheight):
   storeBlockInDB(i)
